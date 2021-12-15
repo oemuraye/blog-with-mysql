e1f4f0d2995e744bb3c29fafa40c1cf9ca5a02c6
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2021-12-15 07:59:51 +0100

    finished the simple blog CRUD

--- app/app.js
+++ app/app.js
@@ -2,13 +2,25 @@ const express = require('express')
 const app = express()
 const routes = require('./routes.js')
 const DB = require('../database/connection')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.use(methodOverride('_method'))
 
-app.use('/', routes )
+
+PORT = process.env.PORT;
+
+app.use('/', routes)
 
 
-app.listen(3000, () => {
-    console.log(` server started on port `)
-  })
+app.listen(PORT, () => {
+  console.log(` server started on port ${PORT} `);
+});
--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const DB = require('../database/connection')
 const router = express.Router()
+const Prevention = require('sqlstring')
 
 router.get('/', (req, res) => {
     DB.query(`SELECT * FROM blogs`, (err, blogPost) => {
@@ -12,6 +13,25 @@ router.get('/', (req, res) => {
         }
     })
 })
+
+router.get('/create', (req, res) => {
+    res.render('create.ejs')
+})
+
+router.post('/create', (req, res) => {
+    const post = req.body
+    DB.query(`INSERT INTO blogs(title, img_url, description) VALUES (
+        ${Prevention.escape(post.title)},
+        ${Prevention.escape(post.img_url)},
+        ${Prevention.escape(post.description)}
+    )`, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        return res.redirect('/')
+    })
+})
+
 router.get('/show/:id', (req, res) => {
     const id = req.params.id
     DB.query(`SELECT * FROM blogs WHERE id = ${id} LIMIT 1`, (err, blog) => {
@@ -20,6 +40,49 @@ router.get('/show/:id', (req, res) => {
         }
         return res.render('show.ejs', {blog})
     })
+})
+
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id
+    const post = req.body
+    DB.query(`SELECT * FROM blogs WHERE id = ${id} LIMIT 1`, (err, blog) => {
+        if (err) {
+            console.log(err)
+        }
+        return res.render('edit.ejs', {blog})
+    })
+})
+
+router.put('/edit/:id', (req, res) => {
+    const id = req.params.id
+    const post = req.body
+    DB.query(
+      `UPDATE blogs
+     SET title = ${Prevention.escape(post.title)},
+     img_url = ${Prevention.escape(post.img_url)},
+     description = ${Prevention.escape(post.description)}
+     WHERE id = ${id}
+     `,
+      (err, blog) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('blog updated');
+        return res.redirect("/");
+      }
+    );
+})
+
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id
+    DB.query(`DELETE FROM blogs WHERE id = ${id}`,  (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('blog deleted')
+        return res.redirect("/")
+      }
+    );
 })
 
 
